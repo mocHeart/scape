ca60b794d85b107e9911ccdb11bdf158cb1afd13
moc <nuc2015@163.com>
2022-07-25 00:26:15 +0800

    font-awesome和ViewUIPlus引入

--- lily/src/App.vue
+++ lily/src/App.vue
@@ -1,10 +1,17 @@
 <template>
+  <menu-header></menu-header>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+
   <router-view></router-view>
 </template>
+
+<script setup>
+import MenuHeader from '@/views/MenuHeader'
+
+</script>
 
 <style lang="scss">
 
--- lily/src/main.js
+++ lily/src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ViewUIPlus from 'view-ui-plus'
+import '@/style/index.scss'
+
 const app = createApp(App)
 app.use(store)
 app.use(router)
+
+app.use(ViewUIPlus)
 
 app.mount('#app')
