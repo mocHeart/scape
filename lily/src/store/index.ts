f1c793bf25f5c8860c31637b5cc8c025c9f52105
moc <nuc2015@163.com>
2022-07-31 22:23:43 +0800

    加入标题栏

--- lily/src/store/index.ts
+++ lily/src/store/index.ts
@@ -2,6 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isShowMenuHeader: true,
+    testText: 's1ds1122'
   },
   getters: {
   },
--- lily/src/views/MenuHeader.vue
+++ lily/src/views/MenuHeader.vue
@@ -1,42 +1,101 @@
 <template>
-    <Menu mode="horizontal" :theme="theme" active-name="1">
-        <MenuItem name="1">
-            <Icon type="ios-paper" />
-            内容管理
-        </MenuItem>
-        <MenuItem name="2">
-            <Icon type="ios-people" />
-            用户管理
-        </MenuItem>
-        <Submenu name="3">
-            <template #title>
-                <Icon type="ios-stats" />
-                统计分析
-            </template>
-            <MenuGroup title="使用">
-                <MenuItem name="3-1">新增和启动</MenuItem>
-                <MenuItem name="3-2">活跃分析</MenuItem>
-                <MenuItem name="3-3">时段分析</MenuItem>
-            </MenuGroup>
-            <MenuGroup title="留存">
-                <MenuItem name="3-4">用户留存</MenuItem>
-                <MenuItem name="3-5">流失用户</MenuItem>
-            </MenuGroup>
-        </Submenu>
-        <MenuItem name="4">
-            <Icon type="ios-construct" />
-            综合设置
-        </MenuItem>
-    </Menu>
+  <div class="menubar" :v-show="store.state.isShowMenuHeader">
+    <div class="left">
+      MOC‘s HOME
+    </div>
+    <div class="center">
+      <Menu mode="horizontal" :theme="theme" active-name="1">
+          <MenuItem name="1">
+              <Icon type="ios-home" />
+              首页
+          </MenuItem>
+          <Submenu name="2">
+              <template #title>
+                  <Icon type="ios-stats" />
+                  关于
+              </template>
+              <MenuItem name="2-1">
+                <Icon type="md-person" />
+                我
+              </MenuItem>
+              <MenuItem name="2-2">
+                <Icon type="md-rose" />
+                主题
+              </MenuItem>
+          </Submenu>
+          <Submenu name="3">
+              <template #title>
+                  <Icon type="ios-stats" />
+                  清单
+              </template>
+              <MenuItem name="3-1">
+                <Icon type="md-person" />
+                归档
+              </MenuItem>
+              <MenuItem name="3-2">
+                <Icon type="md-rose" />
+                标签
+              </MenuItem>
+          </Submenu>
+          <MenuItem name="4">
+              <Icon type="ios-home" />
+              微言
+          </MenuItem>
+          <MenuItem name="5">
+              <Icon type="ios-home" />
+              留言
+          </MenuItem>
+          <MenuItem name="6">
+              <Icon type="ios-home" />
+              实验室
+          </MenuItem>
+          <MenuItem name="7">
+              <Icon type="ios-home" />
+              开往
+          </MenuItem>
+      </Menu>
+    </div>
+    <div class="right">
+      <div>
+        <Icon type="ios-search" size="24" />
+      </div>
+      <div>
+        <Icon type="ios-moon-outline" size="24" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 import { Menu } from 'view-ui-plus'
+import { useStore } from 'vuex'
 
 const theme = ref('light')
+const store = useStore()
+
 </script>
 
 <style lang="scss" scoped>
+.menubar {
+  border: 1px solid red;
+  margin: 0 8vw;
+  display: flex;
+  .left {
+    border: 1px solid green;
+    line-height: 60px;
+  }
+  .center {
+    border: 1px solid blue;
+  }
+  .right {
+    display: flex;
+    border: 1px solid pink;
+    div {
+      line-height: 60px;
+      width: 3vw;
+    }
+  }
+}
 
 </style>
